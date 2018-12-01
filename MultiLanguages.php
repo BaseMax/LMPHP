b52d38515cbcaf02af80e005c30e5f23e523377a
Max Base <MaxBaseCode@Gmail.Com>
2018-12-01 18:42:40 +0330

    Update MultiLanguages.php

--- MultiLanguages.php
+++ MultiLanguages.php
@@ -1,1 +1,23 @@
 <?php
+class MultiLanguages
+{
+	public $languages = [];
+	public $language = null;
+	public $language_default = null;
+	public $words = [];
+	public $word = null;
+	function language($filter=null)
+	{
+		if($filter == null)
+		{
+			return $this->languages;
+		}
+		else
+		{
+			if(language_exists($filter))
+			{
+				return $this->words[$filter];
+			}
+			else{}//#soon #error
+		}
+	}
