10cece484c25eefe6c85deb5212ea44885b862d5
Max Base <MaxBaseCode@Gmail.Com>
2019-01-22 02:24:50 +0330

    Update Sample.php for the new name. (LMPHP)
    Change the name of the class.

--- Sample.php
+++ Sample.php
@@ -1,31 +1,35 @@
 <?php
 /**
 *
-* @Name : MultiLanguages.php
+* @Name : LMPHP (Language management, Multi-Language)
 * @Version : 1.0
 * @Programmer : Max
 * @Date : 2018-12-01
-* @Released under : https://github.com/BaseMax/MultiLanguagesPHP/blob/master/LICENSE
-* @Repository : https://github.com/BaseMax/MultiLanguagesPHP
+* @Released under : https://github.com/BaseMax/LMPHP/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/LMPHP
 *
 **/
-include "MultiLanguages.php";
-$langs = new MultiLanguages();
+include "LMPHP.php";
+$langs = new LMPHP();
+
 $langs -> language_add("en");
 $langs -> language_add("fa","Farsi");
+
 //$langs -> language_remove("en");
+
 $langs -> language_active("fa");
-print_r( $langs -> languages );
 echo $langs -> language;
+
+print_r( $langs -> languages );
 
 $langs -> language_active("en");
 $langs -> word_add("Bye","Good Bye!");
 
 $langs -> language_active("fa");
-
 $langs -> word_add("Hi","Hello!");
 $langs -> word_add("HowAre");
 $langs -> word_add("HowAre","How are you?");//update a word!
+
 print_r( $langs -> words );
 
 echo $langs -> word_get("Bye");//It should get word from active language(fa) , but this is not exists. so will get value from en.
